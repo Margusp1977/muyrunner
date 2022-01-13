688db756f918311c61d822b6764221cc6dc10e8e
Margusp1977 <margusp1977@hotmail.com>
2022-01-13 15:45:25 -0300

    se agrego componente navbar y se actualizo app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from "./components/Footer.jsx";
+import NavBar from "./components/NavBar.jsx";
+
 
 function App() {
     return ( < div className = "App" >
+        <
+        NavBar / >
+
         <
         header className = "App-header" >
         <
@@ -12,13 +17,11 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Tienda de articulos deportivos para el runner. < /p> <a className = "App-link"
+        Tienda de articulos deportivos < /p> <a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
         Learn React < /a> </header >
-        <
-        Footer / >
         <
         /div>
     );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+      <div>
+        <nav >
+          <a > MUY RUNNER</a>
+          <div>
+            <ul >
+              <li >
+                <a >Inicio </a>
+              </li>
+              <li>
+                <a >Productos</a>
+              </li>
+              <li >
+                <a>Sobre nosotros</a>
+              </li>
+              <li>
+                <a >Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+
+  export default NavBar;
